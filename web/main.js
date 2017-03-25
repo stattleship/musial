21705e24d94e3db140131d309042b58e130cf48a
adorsk <adorsk@fas.harvard.edu>
2017-03-25 12:34:59 -0400

    play with random notes

--- web/main.js
+++ web/main.js
@@ -1,50 +1,107 @@
 function main()  {
-  let soundRenderer = new SoundRenderer()
-  let events = loadEvents()
-  soundRenderer.playEvents({events})
+  let score = loadScore()
+  new ScorePlayer().playScore({score})
 }
 
-class SoundRenderer {
+class ScorePlayer {
   constructor(kwargs) {
-    this.synth = new Tone.Synth().toMaster();
+    this.synth = this.genSynth()
   }
-  playEvents(kwargs) {
-    let {events} = kwargs
-    for (let event of events) {
-      this.playEvent({event})
+
+  genSynth() {
+    let synth = new Tone.PolySynth(8).toMaster()
+    return synth
+  }
+
+  playScore(kwargs) {
+    let score = kwargs.score || {}
+    let bpm = score.bpm || 140
+    for (let track of score.tracks) {
+      var renderedTrack = new Tone.Part((time, note) => {
+        this.synth.triggerAttackRelease(
+          note.name,
+          note.duration,
+          time,
+          note.velocity
+        )
+      }, track.notes).start()
     }
-  }
-
-  playEvent(kwargs) {
-    let {event} = kwargs
-    synth.triggerAttackRelease(
-      event.note,
-      event.length
-    )
+    Tone.Transport.start()
   }
 }
 
-function loadEvents() {
-  let events = genEvents()
-  return events
+
+function loadScore() {
+  let score = genScore()
+  return score
 }
 
-function genEvents() {
-  return [
-  ]
-}
-
-function genRandomEvent() {
-  event {
-    'note': 'C' + randomInt({min: 3, max: 5}),
-    'length': '8n'
+function genScore(kwargs) {
+  kwargs = kwargs || {}
+  numNotes = kwargs.numNotes || 3
+  let score = {
+    header: {
+      bpm: 120,
+    },
+    tracks: [
+      {
+        startTime: 0,
+        duration: 127.28854166666662,
+        length: 147,
+        id: 1,
+        name: "Bass",
+        instrumentNumber: 33,
+        instrument: "electric bass (finger)",
+        instrumentFamily: "bass",
+        channelNumber: 0,
+        isPercussion: false,
+        notes: genNotes({numNotes}),
+      }
+    ]
   }
-  return event
+  return score
+}
+
+function genNotes(kwargs) {
+  kwargs = kwargs || {}
+  let curTime = kwargs.startTime || 0
+  let numNotes = kwargs.numNotes || 3
+  let noteDuration = kwargs.noteDuration || 0.25
+  notes = []
+  for (let i = 0; i < numNotes; i++) {
+    note = genNote({
+      time: curTime,
+      duration: noteDuration
+    })
+    notes.push(note)
+    curTime += noteDuration
+  }
+  return notes
+}
+
+function genNote(kwargs) {
+  let {time, duration} = kwargs
+  let note = {
+    "name": genRandomNoteName(),
+    "midi": null,
+    "time": 0,
+    "velocity": 0.6141732283464567,
+    "duration": duration,
+  }
+  return note
+}
+
+function genRandomNoteName() {
+  let LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+  let octave = 4
+  let noteLetter = LETTERS[randomInt({max: LETTERS.length})]
+  let noteName = [noteLetter, octave].join('')
+  return noteName
 }
 
 function randomInt(kwargs) {
-  max = kwargs.max
-  min = kwargs.min or 0
+  let max = kwargs.max
+  let min = kwargs.min || 0
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
